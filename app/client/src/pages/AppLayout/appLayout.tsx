915061b994f939ccd455b4ed697ebdea68c48df6
Ching Chen <piniyu699@gmail.com>
2020-10-21 17:03:52 +0800

    1021

--- app/client/src/pages/AppLayout/appLayout.tsx
+++ app/client/src/pages/AppLayout/appLayout.tsx
@@ -12,6 +12,7 @@ import BlockMeta from '../../components/BlockMeta/blockMeta'
 import ProsCons from '../../components/ProsCons/prosCons'
 import Anchor from '../../components/Anchor/tickerAnchor'
 import TickerComment from '../../components/TickerComment/tickerComment'
+import DiscussionList from '../../components/DiscussionList/discussionList'
 
 const { Header, Sider, Content } = Layout
 
@@ -76,6 +77,7 @@ class AppLayout extends Component {
             <BlockMeta />
             <ProsCons />
             <TickerComment />
+            <DiscussionList />
           </Content>
         </Layout>
       </Layout>
