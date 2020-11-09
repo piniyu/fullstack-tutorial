bd334a5322810a7d067f6b3c78e708deb318913e
Ching Chen <piniyu699@gmail.com>
2020-11-09 18:02:17 +0800

    1109

--- app/client/src/components/Chart/chart.tsx
+++ app/client/src/components/Chart/chart.tsx
@@ -1,3 +1,16 @@
 import React from 'react'
+import { Area, Line, Bar } from '@visx/shape'
 import appleStock, { AppleStock } from '@visx/mock-data/lib/mocks/appleStock'
 import { curveMonotoneX } from '@visx/curve'
+import { GridRows, GridColumns } from '@visx/grid'
+import { scaleTime, scaleLinear } from '@visx/scale'
+import {
+  withTooltip,
+  Tooltip,
+  TooltipWithBounds,
+  defaultStyles,
+} from '@visx/tooltip'
+import { WithTooltipProvidedProps } from '@visx/tooltip/lib/enhancers/withTooltip'
+import { localPoint } from '@visx/event'
+import { LinearGradient } from '@visx/gradient'
+import { max, extent, bisector } from ''
